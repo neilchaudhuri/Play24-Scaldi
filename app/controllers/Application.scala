31551d4f7341813bdc1a14b36429ff740411a19b
Neil Chaudhuri <neil@vidyasource.com>
2015-10-06 21:09:10 -0400

    Formatting

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,7 @@ import scaldi.Injectable._
 class Application(implicit inj: Injector) extends Controller {
   val mailer = inject[Mailer]
   val mode = inject[Mode]
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
