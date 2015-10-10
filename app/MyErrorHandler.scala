d961034c4a4cda716a4352b9815d53dd20cc53e7
Neil Chaudhuri <neil@vidyasource.com>
2015-10-10 18:50:48 -0400

    Add resolver to constructor and place in implicit scope

--- app/MyErrorHandler.scala
+++ app/MyErrorHandler.scala
@@ -2,7 +2,7 @@ package http
 
 import javax.inject.Provider
 
-import controllers.Mailer
+import controllers.{StaticAssetResolver, Mailer}
 import play.api.http.DefaultHttpErrorHandler
 import play.api.mvc.Results._
 import play.api.mvc._
@@ -15,13 +15,18 @@ class MyErrorHandler(env: Environment,
                      config: Configuration,
                      sourceMapper: OptionalSourceMapper,
                      router: () => Router,
-                     mailer: Mailer)
+                     mailer: Mailer,
+                     staticAssetResolver: StaticAssetResolver)
   extends DefaultHttpErrorHandler(env, config, sourceMapper, new Provider[Router] {def get() = router()}) {
+
+  implicit val s = staticAssetResolver
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String = ""): Future[Result] = {
     mailer.mail()
     val result = if (statusCode == play.api.http.Status.NOT_FOUND) {
-      Status(statusCode)("A client error occurred: " + message)
+      NotFound(
+        views.html.error("404")
+      )
     } else {
       Status(statusCode)("A client error occurred: " + message)
     }
