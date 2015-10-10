23549525a25fafc7514a07d384399b2ff6c060b0
Neil Chaudhuri <neil@vidyasource.com>
2015-10-10 18:15:33 -0400

    Replace mailer provider

--- app/MyErrorHandler.scala
+++ app/MyErrorHandler.scala
@@ -15,10 +15,11 @@ class MyErrorHandler(env: Environment,
                      config: Configuration,
                      sourceMapper: OptionalSourceMapper,
                      router: () => Router,
-                     mailerProvider: () => Mailer)
+                     mailer: Mailer)
   extends DefaultHttpErrorHandler(env, config, sourceMapper, new Provider[Router] {def get() = router()}) {
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String = ""): Future[Result] = {
+    mailer.mail()
     val result = if (statusCode == play.api.http.Status.NOT_FOUND) {
       Status(statusCode)("A client error occurred: " + message)
     } else {
