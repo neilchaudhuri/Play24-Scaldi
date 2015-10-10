f835e4f953dee6640d667dffa6f5261469102f26
Neil Chaudhuri <neil@vidyasource.com>
2015-10-10 18:49:28 -0400

    Add implicit resolver

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,8 @@ class Application(implicit inj: Injector) extends Controller {
   val mailer = inject[Mailer]
   val mode = inject[Mode]
 
+  implicit val staticAssetResolver: StaticAssetResolver = inject[StaticAssetResolver]
+
   def index = Action {
     mailer.mail()
     Ok(views.html.index("Your new application is ready."))
